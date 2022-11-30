3fa9fac15dd6341188ad13287a4afc9dab6a5083
Raman <ramanmcreates@gmail.com>
2022-11-30 13:54:05 -0500

    commit tree b2974d9098e341696ab081f0f55cac9205ddb1d1

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,18 @@
 class Actor < ActiveRecord::Base
+    has_many :characters
+    has_many :shows, through: :characters
+
+    def full_name
+        #returns actors full name
+        self.first_name + " " + self.last_name
+    end 
+
+    def list_roles
+        #returns a list of roles for the actor using the character name and show name
+        self.character + self.character.show
+    end 
+
+
+   
   
 end
